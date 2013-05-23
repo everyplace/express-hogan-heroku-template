8b98f670ee4ee6ced5cfb5b7ce6acf80b75b0227
Erin Sparling <erin@everyplace.net>
2013-05-23 12:02:19 +0100

    Commented, slightly.

--- app.js
+++ app.js
@@ -22,16 +22,18 @@ app.configure(function(){
 app.configure('development', function(){
   app.use(express.errorHandler());
 });
-
-console.log(testMiddleware);
   
+
+//configure routes  
 app.get('/', routes.index);
 app.get('/test', testMiddleware.index, routes.test);
 
+//configure optional debug information
 if(process.env.DEBUG == 'true') {
   console.log("DEBUG ENABLED");
 };
 
+//initiate the app server
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
